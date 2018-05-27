e898b468c1599fbafd316a93b2f2ef8117ea38a3
Ian Goldfarb <ian@supplyhub.com>
2018-05-26 17:12:33 -0700

    updated public apis and such for a new npm package 0.3.0, not quite ready to call this 1.0 yet, but it's looking good

--- public_api.ts
+++
@@ -1,6 +0,0 @@
-export * from './src/app/elements/standard-input/standard-input.module';
-export * from './src/app/elements/card/card.module';
-export * from './src/app/elements/button/button.module';
-export * from './src/app/elements/toggle/toggle.module';
-export * from './src/app/elements/slider/slider.module';
-export * from './src/app/elements/checkbox/checkbox.module';
---
+++ src/public_api.ts
@@ -0,0 +1,7 @@
+export * from './app/elements/input/input.module';
+export * from './app/elements/card/card.module';
+export * from './app/elements/button/button.module';
+export * from './app/elements/toggle/toggle.module';
+export * from './app/elements/slider/slider.module';
+export * from './app/elements/checkbox/checkbox.module';
+export * from './app/elements/radio/radio.module';
