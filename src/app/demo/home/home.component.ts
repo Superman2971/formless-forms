054794a6f0806c8d0909aba175610261bdf3fea2
Ian Goldfarb <ian@supplyhub.com>
2018-07-09 13:19:12 -0700

    work on sample form and a small update to radio element

--- src/app/demo/home/home.component.ts
+++ src/app/demo/home/home.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  form: any = {
+    acceptedTerms: false,
+    email: null,
+    gender: false,
+    human: null,
+    name: null,
+    score: 100
+  };
+  dirty: boolean = false;
+
   constructor() {}
+
+  clear() {
+    this.form = {
+      acceptedTerms: false,
+      email: null,
+      gender: null,
+      human: false,
+      name: null,
+      score: 100
+    };
+    this.dirty = false;
+  }
+
+  submit() {
+    this.dirty = true;
+    console.log(this.form);
+  }
 }
