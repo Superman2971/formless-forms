67ef425bd7fa6dc740b03871d409d277c44ac7d2
Ian Goldfarb <ian@supplyhub.com>
2018-07-16 23:03:58 -0700

    completed the input demo but I think a better more concise version can be created

--- src/app/demo/components/input-demo/input-demo.component.ts
+++ src/app/demo/components/input-demo/input-demo.component.ts
@@ -14,97 +14,98 @@ export class InputDemoComponent {
     name: 'placeholder',
     type: 'string',
     default: 'null',
-    description: ''
+    description: 'Optionally provide the placeholder which will be used as the label for your input.'
   }, {
     name: 'init',
     type: 'string',
     default: 'null',
-    description: ''
+    description: 'Used to initialize and can be used to update the input text.'
   }, {
     name: 'optional',
     type: 'boolean',
     default: 'false',
-    description: ''
+    description: 'When true the field will no longer be required and so the validation will not show.'
   }, {
     name: 'hideOptional',
     type: 'boolean',
     default: 'false',
-    description: ''
+    description: 'When true the (optional) flag on the input label will not show.'
   }, {
     name: 'dirty',
     type: 'boolean',
     default: 'false',
-    description: ''
+    description: 'When true the validations will display.'
   }, {
     name: 'minlength',
     type: 'number',
     default: 'null',
-    description: ''
+    description: 'Optionally provide the minmum length for input text. This will show error validation if not reached.'
   }, {
     name: 'maxlength',
     type: 'number',
     default: '200',
-    description: ''
+    description: 'Optionally provide the maximum length for input text. Will not allow text longer than maxlength.'
   }, {
     name: 'disabled',
     type: 'boolean',
     default: 'false',
-    description: ''
+    description: 'When true pointer events are disabled and input would not be accessible.'
   }, {
     name: 'description',
     type: 'string',
     default: 'null',
-    description: ''
+    description: 'When provided the description will display beneath the input until field is dirty and valid.'
   }, {
     name: 'numberType',
     type: 'boolean',
     default: 'false',
-    description: ''
+    description: 'When true input type will be changed to number and only accept numbers for field.'
   }, {
     name: 'email',
     type: 'boolean',
     default: 'false',
-    description: ''
+    description: 'When true the input will need to match our email regex validation to be valid.'
   }, {
     name: 'hiddenType',
     type: 'boolean',
     default: 'false',
-    description: ''
+    description: 'When true the input type will initialize as password to hide the text. Ability to show/hide the text will be available.'
   }, {
     name: 'matchThis',
     type: 'string',
     default: 'null',
-    description: ''
+    description: 'When provided the text input will only be valid when matching the string/variable set in the matchThis property.'
   }, {
     name: 'textarea',
     type: 'boolean',
     default: 'false',
-    description: ''
+    description: 'When true the input will become a textarea which can have multiple rows.'
   }, {
     name: 'rows',
     type: 'number',
     default: 'null',
-    description: ''
-  }, {
-    name: 'optionalStyles',
-    type: 'string',
-    default: 'null',
-    description: ''
-  }, {
-    name: 'inputStyles',
-    type: 'string',
-    default: 'null',
-    description: ''
+    description: 'Optionally set the rows for a textarea input to display. Textarea property must be set to true for this to work.'
   }, {
     name: 'regex',
     type: 'string',
     default: 'null',
-    description: ''
+    description: 'Optionally can provide custom regex pattern as a required validation for the input.'
   }, {
     name: 'regexMessage',
     type: 'string',
     default: 'null',
-    description: ''
+    description: `When provided the custom regex validation will display this text when not met.
+      Regex property is required for this to display.`
+  }, {
+    name: 'optionalStyles',
+    type: 'string',
+    default: 'null',
+    description: 'Optionally may provide styles for the overall container here.'
+  }, {
+    name: 'inputStyles',
+    type: 'string',
+    default: 'null',
+    description: 'Optionally may provide styles directly to the input element here. Does not work for textarea.'
   }, {
     name: 'value',
     type: 'event',
--- src/app/demo/markdown-code-samples/input_html.md
+++ src/app/demo/markdown-code-samples/input_html.md
@@ -1,3 +1,40 @@
 ``` html
-<div>SOMETHING HERE</div>
+<div class="title">Standard Input</div>
+<formless-input [placeholder]="'placeholder'" (value)="placeholder=$event"></formless-input>
+<formless-input [placeholder]="'init'" [init]="'Initalized Text'" (value)="init=$event"></formless-input>
+<formless-input [placeholder]="'optional'" [optional]="true" (value)="optional=$event"></formless-input>
+<formless-input [placeholder]="'hideOptional'" [optional]="true"  [hideOptional]="true" (value)="hideOptional=$event"></formless-input>
+<formless-input [placeholder]="'dirty'" [dirty]="true" (value)="dirty=$event"></formless-input>
+<formless-input [placeholder]="'value'" (value)="value=$event"></formless-input>
+<formless-card>Value Input Results: {{ value }}</formless-card>
+
+<div class="title">Basic Input Validations</div>
+<formless-input [placeholder]="'minlength'" [minlength]="4" (value)="minlength=$event"></formless-input>
+<formless-input [placeholder]="'maxlength'" [maxlength]="10" (value)="maxlength=$event"></formless-input>
+<formless-input [placeholder]="'disabled'" [disabled]="true" (value)="disabled=$event"></formless-input>
+<formless-input [placeholder]="'description'" [description]="'description is displayed here'" (value)="description=$event"></formless-input>
+
+<div class="title">Typed Input</div>
+<formless-input [placeholder]="'numberType'" [numberType]="true" (value)="numberType=$event"></formless-input>
+<formless-input [placeholder]="'email'" email="true" (value)="email=$event"></formless-input>
+<formless-input [placeholder]="'hiddenType'" hiddenType="true" (value)="hiddenType=$event"></formless-input>
+<formless-input [placeholder]="'matchThis (match hiddenType)'" [matchThis]="hiddenType" (value)="matchThis=$event"></formless-input>
+
+<div class="title">Text Area Input</div>
+<formless-input [placeholder]="'textarea'" [textarea]="true" (value)="textarea=$event"></formless-input>
+<formless-input [placeholder]="'rows'" [textarea]="true" [rows]="7" (value)="rows=$event"></formless-input>
+
+<div class="title">Custom Styles and Regex Validation</div>
+<formless-input
+  [placeholder]="'regex and regexMessage (phone regex)'"
+  regex="^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$"
+  regexMessage="*Does not match the pattern for a phone number"
+  (value)="regex=$event"
+></formless-input>
+<div>optionalStyles and inputStyles</div>
+<formless-input
+  [optionalStyles]="{'min-height': 0}"
+  [inputStyles]="{'border': '1px solid #D5DAE5', 'border-radius': '2px', 'font-size': '12px',	'font-weight': '500', 'height': '36px', 'margin': 0, 'padding': '0 0 0 16px'}"
+  (value)="custom=$event"
+></formless-input>
 ```
